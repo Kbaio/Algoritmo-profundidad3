be27a0c2e3aeff7748ee09269ece9e3d1198b10b
David Salazar <50851287+Kbaio@users.noreply.github.com>
2022-08-04 13:20:50 -0600

    Add files via upload

---
+++ Algoritmo Profundidad 3/Algoritmo Profundidad 3/Algoritmo Profundidad 3.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+internal class Program
+{
+ 
+    //Hipotesis: Al ser un algoritmo de profundidad 3 con tantas iteraciones va a durar una increible cantidad de tiempo antes de terminar.
+
+    //Luego de dejar el programa por unos 21 minutos logro realizar 90376820 iteraciones
+
+    static void Main()
+    {
+        Console.WriteLine("The application started at {0:HH:mm:ss.fff}", DateTime.Now);
+        int num = 0;
+
+        for (int i = 0; i < 10000; i++)
+        {
+            for (int j = 0; j < 10000; j++)
+            {
+                for (int k = 0; k < 10000; k++)
+                {
+                    num += 1;
+                    Console.WriteLine("Iteracion numero: " + num);
+                }
+
+            }
+        }
+    }
+}
+
